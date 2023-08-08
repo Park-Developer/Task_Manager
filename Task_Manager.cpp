87649a88027a70c438cb9fde8af292c191a99aaa
Wonho Park <parkwonho94@gmail.com>
2023-08-08 22:49:00 +0900

    초기 커밋.

---
+++ Task_Manager.cpp
@@ -0,0 +1,80 @@
+﻿#include <windows.h>
+#include <tchar.h>
+#define MAX_LOADSTRING 100
+
+// 전역 변수
+HINSTANCE hInst;                               // 현재 인스턴스입니다.
+TCHAR szTitle[MAX_LOADSTRING] = _T("Task Manager");   // 제목 표시줄 텍스트입니다.
+TCHAR szWindowClass[MAX_LOADSTRING] = _T("Task Manager"); // 기본 창 클래스 이름입니다.
+
+// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+
+
+int APIENTRY  _tWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPTSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+
+{
+	// 윈도우 등록
+	WNDCLASSEX wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+
+	RegisterClassEx(&wcex);
+
+	hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
+	// 윈도우 생성
+	HWND hWnd = CreateWindow(szWindowClass, szTitle,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0,
+		CW_USEDEFAULT, 0,
+		nullptr, nullptr, hInstance, nullptr);
+	// 윈도우 보이기, 갱신
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	MSG msg;
+	while (GetMessage(&msg, nullptr, 0, 0))  // 기본 메시지 루프입니다:
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
+//윈도우 프로시저
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_DESTROY:
+		{
+		PostQuitMessage(0);
+		}
+		break;
+	case WM_PAINT:
+		{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		// 여기에 hdc를 사용하는 그리기 코드를 추가하낟.
+		TextOut(hdc, 100, 100, _T("Task Manager"), lstrlen("Task Manager"));
+		EndPaint(hWnd, &ps);
+		}
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
